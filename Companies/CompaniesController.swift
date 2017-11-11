3a3ff11282c842d78dbd936777739110ed57f87e
bwong <bwong@mwongs-MacBook-Pro.local>
2017-11-10 20:02:10 -0800

    Created Company Model. Added more UI to companyController

--- Companies/CompaniesController.swift
+++ Companies/CompaniesController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class CompaniesController: UIViewController {
+class CompaniesController: UITableViewController {
+    
+    let cellID = "cellID"
+    
+    let companies = [
+        Company(name: "Apple", founded: Date()),
+        Company(name: "Google", founded: Date()),
+        Company(name: "Salesforce", founded: Date())
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,8 +24,17 @@ class CompaniesController: UIViewController {
         
         view.backgroundColor = UIColor.white
         
+        setupTableViewSettings()
+        
         let plusImage = UIImage(named: "plus")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: plusImage?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany)) 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: plusImage?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+    }
+    
+    func setupTableViewSettings() {
+        self.tableView.separatorStyle = .none
+        self.tableView.backgroundColor = UIColor.darkBlue
+        self.tableView.tableFooterView = UIView() //blank view
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
     
     func handleAddCompany() {
@@ -25,13 +42,49 @@ class CompaniesController: UIViewController {
     }
     
     func setupNavigationStyle() {
-        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
         
         navigationItem.title = "Companies"
         
         navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.barTintColor = UIColor.lightRed
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let company = companies[indexPath.row]
+        
+        cell.textLabel?.text = company.name
+//        cell.detailTextLabel.text = company.founded
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        cell.backgroundColor = UIColor.tealColor
+        
+        return cell
     }
 
 }
---
+++ Companies/Company.swift
@@ -0,0 +1,16 @@
+//
+//  Company.swift
+//  Companies
+//
+//  Created by bwong on 11/10/17.
+//  Copyright © 2017 mwong. All rights reserved.
+//
+
+import Foundation
+
+struct Company {
+    
+    let name: String
+    let founded: Date
+    
+}
